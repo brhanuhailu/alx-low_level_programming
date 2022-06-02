1a5bd9fbb2c10db7f0342970e75a24304292eed9
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-02 09:04:11 -0700

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This is main - Entry point
+ *
+ *At end Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
