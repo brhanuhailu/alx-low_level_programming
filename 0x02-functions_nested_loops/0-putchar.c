a94c9ca74acda7d5240965058435f53bbbe70ee7
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-06 22:56:06 -0700

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char main[] = "_putchar";
+	int i = 0;
+
+	while (main[i] != '\0')
+	{
+		_putchar(main[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
