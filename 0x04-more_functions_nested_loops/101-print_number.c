e367ea95ca8919a2e85f68b8b883c12002374f92
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-09 07:28:29 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,41 +7,41 @@
  */
 void print_number(int n)
 {
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
+long m; /* power of 10 */
+int c; /* boolean check */
+long num; /* convert int to long */
 
-	num = n;
-	/* negatives */
-	if (num < 0)
-	{
-		num *= -1;
-		_putchar('-');
-	}
+num = n;
+/* negatives */
+if (num < 0)
+{
+num *= -1;
+_putchar('-');
+}
 
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
+/* count up */
+m = 1;
+c = 1;
+while (c)
+{
+if (num / (m * 10) > 0)
+m *= 10;
+else
+c = 0;
+}
 
-	/* count down */
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+/* count down */
+while (num >= 0)
+{
+if (m == 1)
+{
+_putchar(num % 10 + '0');
+num = -1;
+}
+else
+{
+_putchar((num / m % 10) + '0');
+m /= 10;
+}
+}
 }
