50ed950f725882eb6c45db1cec9d9bb98d5b715b
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-20 10:56:36 -0700

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	c = 0;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				break;
+			}
+			b++;
+		}
+		if (accept[b] == '\0')
+			break;
+		a++;
+	}
+	return (c);
+}
