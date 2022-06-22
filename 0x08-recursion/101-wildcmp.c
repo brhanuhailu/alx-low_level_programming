2be9f0b26ecfef72235b82bb8e48ce75992e48a6
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-22 08:20:12 -0700

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,9 +14,13 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '\0' || *s2 == '\0')
 	{
 		if (*s1 == '\0' && *s2 == '*')
+		{
 			return wildcmp(s1, ++s2);
+		}
 		else if (*s1 == '*' && *s2 == '\0')
+		{
 			return wildcmp(++s1, s2);
+		}
 		return (0);
 	}
 
@@ -27,7 +31,9 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '*')
 	{
 		if (*(s1 + 1) == '*')
+		{
 			return wildcmp(++s1, s2);
+		}
 		else
 		{
 			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
@@ -36,7 +42,9 @@ int wildcmp(char *s1, char *s2)
 	else if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
+		{
 			return wildcmp(s1, ++s2);
+		}
 		else
 		{
 			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
@@ -50,9 +58,13 @@ int wildcmp(char *s1, char *s2)
 int findsrc(char *s, char c, int i, int p)
 {
 	if (*(s + i) == '\0')
+	{
 		return (p + 1);
+	}
 	else if (*(s + i) == c || *(s + i) == '*')
+	{
 		p = i;
+	}
 
 	return (findsrc(s, c, i + 1, p));
 }
