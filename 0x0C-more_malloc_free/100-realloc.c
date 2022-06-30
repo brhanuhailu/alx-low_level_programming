63d8d865be9ddf11565a7ba1054f9f5070d6be5d
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-06-30 09:18:33 -0700

    Create 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - a function that reallocates a memory block.
+ * @ptr : void pointer to the allocated memory area .
+ * @old_size : the old size of the allocated memory.
+ * @new_size : the new size of reallocated memory.
+ * Return: void pointer.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+/* allocating new dynamic memory without initialization */
+	if (new_size > old_size)
+	{
+		free(ptr);
+			ptr = (int *) malloc(new_size);
+	if (ptr == NULL)
+	return (NULL);
+	else
+	return (ptr);
+	}
+/*the call is equivalent to malloc(new_size)*/
+	if (ptr == NULL)
+	{
+		return (ptr = (int *) malloc(new_size));
+
+			if  (ptr == NULL)
+				return (NULL);
+	}
+/* returns a Void Pointer */
+
+	if (new_size == old_size)
+	return (ptr);
+/* freeing Ptr */
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+			return (NULL);
+	}
+
+	return (ptr);
+}
