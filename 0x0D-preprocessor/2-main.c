b96fe74a207496062cd0d978942d4895848cb593
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-07-04 00:59:32 -0700

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the name of the file
+ * using common predefined macros
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
