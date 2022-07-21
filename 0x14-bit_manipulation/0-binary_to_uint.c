88c82d2d4cbc86272f37b626a3549e99ca6ae8ce
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-07-21 04:28:36 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - convert a binary string to an integer
  * @b: the string to convert
@@ -10,10 +9,8 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-
 	if (!b)
 		return (0);
-
 	while (*b)
 	{
 		n <<= 1;
