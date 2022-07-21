c842582c6f785f3befdc2a174f1bd63bf17ef597
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-07-21 03:33:44 -0700

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_binary - print a number in binary
+ * @n: the number to print
+ */
+void _print_binary(unsigned long int n)
+{
+	if (n)
+	{
+		_print_binary(n >> 1);
+		_putchar('0' + (n & 1));
+	}
+}
+
+/**
+ * print_binary - print a number in binary
+ * @n: the number to print
+ */
+void print_binary(unsigned long int n)
+{
+	if (n)
+		_print_binary(n);
+	else
+		_putchar('0');
+}
