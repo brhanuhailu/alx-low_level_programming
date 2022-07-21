90ce23ee83e0fd2e09290230577c7629723cbeb2
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-07-21 03:38:14 -0700

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the unique bits in two values
+ * @n: the first integer
+ * @m: the second integer
+ *
+ * Return: the number of bits that differ between n and m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	n ^= m;
+	m = 0;
+	while (n)
+	{
+		m += 1 & n;
+		n >>= 1;
+	}
+	return (m);
+}
