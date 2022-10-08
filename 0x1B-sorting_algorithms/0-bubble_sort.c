9e443611de127c85823ba14fff1d4ce395830387
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-10-08 10:40:06 -0700

    Create 0-bubble_sort.c

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, k;
+
+	if (!array || !size)
+		return;
+
+	i = 0;
+	while (i < size)
+	{
+		for (k = 0; k < size - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
